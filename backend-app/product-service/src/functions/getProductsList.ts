db3f7b491744f975b96663a0bcedf2472c69a481
Maxim Semikov <maxim.semikov87@gmail.com>
2025-03-01 22:11:07 +0500

    feat: Task 4.2 Integrate the getProductsList lambda to return via GET /products request a list of products from the database (joined stocks and products tables).

--- backend-app/product-service/src/functions/getProductsList.ts
+++ backend-app/product-service/src/functions/getProductsList.ts
@@ -1,20 +1,56 @@
-import { APIGatewayProxyResult } from "aws-lambda";
-import { mockProducts } from "./products";
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
-export const handler = async (
-): Promise<APIGatewayProxyResult> => {
+const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+const docClient = DynamoDBDocumentClient.from(ddbClient);
+
+export const handler: APIGatewayProxyHandler = async () => {
   try {
+
+    //get data from DB
+    const [productsResponse, stocksResponse] = await Promise.all([
+        docClient.send(
+            new ScanCommand({
+              TableName: process.env.PRODUCTS_TABLE,
+        })
+    ),
+
+      docClient.send(
+          new ScanCommand({
+            TableName: process.env.STOCKS_TABLE,
+          })
+      )
+    ])
+
+    // Join products with stocks
+    const products = productsResponse.Items?.map((product) => {
+      const stock = stocksResponse.Items?.find(
+          (stock) => stock.product_id === product.id
+      );
+
+      return {
+        ...product,
+        count: stock?.count || 0,
+      };
+    });
+
     return {
       statusCode: 200,
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials": true,
       },
-      body: JSON.stringify(mockProducts),
+      body: JSON.stringify(products),
     };
   } catch (error) {
+    console.error("Error fetching products:", error);
     return {
       statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
       body: JSON.stringify({ message: "Internal server error" }),
     };
   }
