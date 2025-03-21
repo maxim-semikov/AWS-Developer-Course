726eca6d86b5dd0ef9a9afdc01f621823857a9f3
Maxim Semikov <maxim.semikov87@gmail.com>
2025-03-21 22:29:07 +0500

    feat: Task 7.3 Request from the client application to the /import path of the Import Service should have Basic Authorization header

--- frontend-app/src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ frontend-app/src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -62,9 +62,9 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
         params: {
           name: encodeURIComponent(file.name),
         },
-        headers: {
+        headers: authorizationToken ? {
           Authorization: `Basic ${authorizationToken}`,
-        },
+        } : {},
       });
 
       console.log("File to upload: ", file.name);
