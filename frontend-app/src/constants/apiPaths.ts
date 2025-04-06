5bea9a741e80a498ce9c23e65adf107bb7ad7a65
Maxim Semikov <maxim.semikov87@gmail.com>
2025-04-06 10:08:06 +0500

    Task 8.3 Integrate Cart service with FE repository

--- frontend-app/src/constants/apiPaths.ts
+++ frontend-app/src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://bfy61ruba7.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://frks5bel29.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://ax3gt6lrzqyks2f64frhbfn5oa0btjhw.lambda-url.eu-central-1.on.aws/api",
 };
 
 export default API_PATHS;
