71c2d17b7db8c5b32854d3a2013d89a2476a3cf8
Maxim Semikov <maxim.semikov87@gmail.com>
2025-04-13 11:45:58 +0500

    feat(Task 9.2) Integrate Cart service with FE repository

--- frontend-app/src/constants/apiPaths.ts
+++ frontend-app/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "http://maxim-semikov-cart-api-app-prod.eu-central-1.elasticbeanstalk.com/api/profile/cart",
+  order: "http://maxim-semikov-cart-api-prod.eu-central-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://bfy61ruba7.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://frks5bel29.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "http://maxim-semikov-cart-api-app-prod.eu-central-1.elasticbeanstalk.com/api",
+  cart: "http://maxim-semikov-cart-api-prod.eu-central-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
